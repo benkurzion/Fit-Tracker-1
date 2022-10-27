a1c9ffd11f66de9e138219dd994f3854da0c17c4
benkurzion <benkurzion@gmail.com>
2022-10-27 14:17:21 -0400

    With some js now

---
+++ public/javascripts/app.js
@@ -0,0 +1,15 @@
+var category = "";
+
+var dataEntry = {
+    vvalue: 0,
+    unit:"",
+    date: 0
+};
+
+function newCategory(category){
+
+}
+
+function enterData(category, value, unit, date){
+
+}
