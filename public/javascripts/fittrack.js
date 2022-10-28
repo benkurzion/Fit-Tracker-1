b080ae0ba39a6e38b5c778a9f7a3115a57f9dbae
benkurzion <benkurzion@gmail.com>
2022-10-27 21:54:02 -0400

    Testing Commit

---
+++ public/javascripts/fittrack.js
@@ -0,0 +1,16 @@
+var category = "";
+
+
+var dataEntry = {
+    value: 0,
+    unit:"",
+    date: 0
+};
+
+function newCategory(category){
+
+}
+
+function enterData(category, value, unit, date){
+
+}
